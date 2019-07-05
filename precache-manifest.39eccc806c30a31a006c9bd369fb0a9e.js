fef452fd483a6ba7aff65d77888b16b7fae71bd7
jakubk3697 <kupson3697@gmail.com>
2019-07-05 23:44:48 +0200

    Updates

---
+++ precache-manifest.39eccc806c30a31a006c9bd369fb0a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e840d36529515c5c6d2564d03fe183d1",
+    "url": "/Weather-app-react/index.html"
+  },
+  {
+    "revision": "1468766c7129b4a7b519",
+    "url": "/Weather-app-react/static/css/main.1b4490ec.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Weather-app-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1468766c7129b4a7b519",
+    "url": "/Weather-app-react/static/js/main.a3f0dc68.chunk.js"
+  },
+  {
+    "revision": "9324d6e9186bbf0e1b84",
+    "url": "/Weather-app-react/static/js/runtime~main.d686dbfd.js"
+  },
+  {
+    "revision": "817c770ba36bcea3ed5552dae4c10424",
+    "url": "/Weather-app-react/static/media/day.817c770b.jpg"
+  },
+  {
+    "revision": "77e489fe3c8b183695c8ab28a1a1f01b",
+    "url": "/Weather-app-react/static/media/night.77e489fe.jpg"
+  }
+]);
